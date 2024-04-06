01b3d0f5b0dbfffdb613d3359ef7391c63efa3b6
Aaryan Dave <aaryan.r.dave@gmail.com>
2024-04-06 14:43:57 -0400

    Clean repo.

--- train_ltc.py
+++ train_ltc.py
@@ -43,7 +43,7 @@ legend_handles = wiring.draw_graph(draw_labels=True, neuron_colors={"command": "
 plt.legend(handles=legend_handles, loc="upper center", bbox_to_anchor=(1, 1))
 sns.despine(left=True, bottom=True)
 plt.tight_layout()
-plt.savefig("initial_wiring.png")
+plt.savefig("./results/initial_wiring.png")
 
 # Visualize before training
 prediction = model(data_x).numpy()
@@ -51,8 +51,8 @@ plt.figure(figsize=(6, 4))
 plt.plot(data_y, label="Target output")
 plt.plot(prediction[:, 0], label="LTC Prediction", linestyle="dashed")
 plt.title("Before training")
-plt.legend(loc="upper right")
-plt.savefig("before_training.png")
+plt.legend(loc="upper left")
+plt.savefig("./results/before_training.png")
 
 # Train the model
 hist = model.fit(x=data_x, y=data_y, batch_size=1, epochs=100, verbose=1)
@@ -62,17 +62,24 @@ plt.figure(figsize=(6, 4))
 plt.plot(hist.history["loss"], label="Training loss")
 plt.legend(loc="upper right")
 plt.xlabel("Training steps")
-plt.savefig("training_loss.png")
+plt.savefig("./results/training_loss.png")
 
 # Visualize after training
 prediction = model(data_x).numpy()
 plt.figure(figsize=(6, 4))
 plt.plot(data_y, label="Target output")
 plt.plot(prediction[:, 0], label="LTC Prediction", linestyle="dashed")
-plt.legend(loc="upper right")
+plt.legend(loc="upper left")
 plt.title("After training")
-plt.savefig("after_training.png")
+plt.savefig("./results/after_training.png")
 
-# Save the model
-model.save(f'stock_model_{(16, 1)}.h5')
+# Save the model in keras format
+model.save("ltc_model.keras")
 print("Model saved successfully.")
+
+# predict next 30 days
+data_next_seq = data.iloc[-sequence_length:].values
+data_next_seq = np.expand_dims(data_next_seq, axis=0)
+
+prediction = model.predict(data_next_seq)
+print("Prediction for next 30 days:", prediction)
